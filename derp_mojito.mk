db7bb14b006a5c13bd6358080da2bca2611f97dd
zhantech <hifzhan41@gmail.com>
2021-08-10 15:10:50 +0700

    mojito: Update Coral FP Agustus 2021

--- derp_mojito.mk
+++ derp_mojito.mk
@@ -35,8 +35,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Build fingerprint
-BUILD_FINGERPRINT := google/coral/coral:11/RQ3A.210605.005/7349499:user/release-keys
-BUILD_DESCRIPTION := coral-user 11 RQ3A.210605.005 7349499 release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RQ3A.210805.001.A1/7474174:user/release-keys
+BUILD_DESCRIPTION := coral-user 11 RQ3A.210805.001.A1 7474174 release-keys
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
