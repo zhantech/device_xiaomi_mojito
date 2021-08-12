2f1859df2c360115f2237df656190bb1fbe3d5d6
zhantech <hifzhan41@gmail.com>
2021-08-12 03:08:26 +0000

    mojito: Add Bloatware and Remove Some Packages

--- device.mk
+++ device.mk
@@ -235,3 +235,9 @@ PRODUCT_BOOT_JARS += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
+
+# Bloatware
+PRODUCT_PACKAGES += \
+    GCam \
+    GalleryGo \
+    RemovePackages
